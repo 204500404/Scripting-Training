3ba62b053496224adce84a03439af7516a1f12eb
204500404 <178456428+204500404@users.noreply.github.com>
2025-04-25 23:08:04 +0200

    Commit n°2 : Amélioration du script maj.sh

--- bash/maj.sh
+++ bash/maj.sh
@@ -2,27 +2,42 @@
 
 GREEN="\e[32m"
 RED="\e[31m"
-NC="\e[0m" # Nocolor
+NC="\e[0m" #Nocolor
 
 if [ "$UID" -ne 0 ]; then
-    echo -e "${RED}Erreur : Ce script doit être exécuté en tant que root (sudo).${NC}" >&2
-    exit 1
+	echo "${RED}Erreur : Ce script doit être éxecuter en tant que root (sudo).${NC}" >&2
+	exit 1
 fi
 
-echo -e "${GREEN}Vous exécutez bien ce script en tant que root, le script peut commencer.${NC}"
+
+echo "${GREEN}Vous executez bien ce script en tant que root le script peut commencer${NC}"
 
 LOGFILE="/var/log/maj_systeme.log"
 
 if [ ! -f "$LOGFILE" ]; then
-    touch "$LOGFILE" && chmod 600 "$LOGFILE"
+	touch "$LOGFILE" && chmod 600 "$LOGFILE"
+
+fi
+
+mkdir -p /var/log/maj_system.log
+
+echo -e "${GREEN}Verification de la connexion...${NC}"
+
+ping -c 4 8.8.8.8 > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo -e "${RED} Connexion réseau absente, Abandon du script. ${NC}" >&2
+	exit 1
 fi
 
 echo -e "${GREEN}Mise à jour du système en cours...${NC}"
-echo "Début de mise à jour : $(date '+%a %d/%m/%Y %H:%M:%S')" >> "$LOGFILE"
+
+echo "Début de Mise à jour : $(date "+%A %d/%m/%Y %H:%M:%S")" >> "$LOGFILE"
+
 
 apt update >> "$LOGFILE" 2>&1
 apt upgrade -y >> "$LOGFILE" 2>&1
-apt autoremove -y >> "$LOGFILE" 2>&1
+
+apt autoremove -y >> "$LOGFILE"  2>&1
 apt clean >> "$LOGFILE" 2>&1
 
-echo -e "${GREEN}Mise à jour terminée avec succès${NC}"
+echo "Mise à jour terminée avec succès"
