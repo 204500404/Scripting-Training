ef3f8b46b22a972bbe84f763f19afa746158b455
204500404 <178456428+204500404@users.noreply.github.com>
2025-04-27 18:34:46 +0200

    Correction + Amelioration du Script de nettoyage du repertoire /tmp

--- bash/rm-oldfile.sh
+++ bash/rm-oldfile.sh
@@ -9,9 +9,17 @@ if [ "$UID" -ne 0 ]; then
         echo -e "${RED}Pour qu'il fonctionne, vous devez executer ce script en tant que root (sudo)${NC}"
         exit 1
 fi
-
+#Verifie que le repertoire /tmp existe
 if [ ! -d /tmp ]; then
         echo -e "${RED}Le repertoire /tmp n'existe pas.${NC}"
+        echo "Creation du repertoire /tmp..."
+        mkdir -p /tmp
+fi
+#Verifie que le repertoire /var/log existe
+if [ ! -d /var/log ]; then
+        echo -e "${RED}Le repertoire /var/log n'existe pas.${NC}"
+        echo "Creation du repertoire /var/log..."
+        mkdir -p /var/log/
 fi
 
 echo -e "${GREEN}Vous executez bien ce script en tant que root, le script peut commencer${NC}"
@@ -19,13 +27,12 @@ echo -e "${GREEN}Vous executez bien ce script en tant que root, le script peut c
 #Création d'un fichier de LOG
 
 LOGFILE="/var/log/script_rm-oldfile.log"
-mkdir -p /var/log/
 touch "$LOGFILE"
 echo "$(date "+%Y/%m/%d %H:%M:%S") : Nettoyage effectué." >> "$LOGFILE"
 
 #Nettoyage de /tmp
 
-echo "nettoyage de /tmp en cours..."
+echo "Nettoyage de /tmp en cours..."
 find /tmp -mindepth 1 -mtime +7 -delete
 
 #Message de confirmation
