fcd765081e73f72294d37f120ac604760e23e8a3
204500404 <178456428+204500404@users.noreply.github.com>
2025-05-01 18:25:45 +0200

    Ajout de commentaire, supression des caracteres causant des pb d'affichage (accents etc..)

--- bash/check_critical_daemons/check_critical_daemons.sh
+++ bash/check_critical_daemons/check_critical_daemons.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Definition des couleurs pour l'affichage
 RED='\e[31m'
 GREEN='\e[32m'
 NC='\e[0m'
@@ -8,29 +9,30 @@ NC='\e[0m'
 
 LISTE="DaemonsList.txt"
 
+# Verification de l'existence du fichier
 if [ ! -f "$LISTE" ]; then
     echo -e "${RED}ERREUR: Le fichier $LISTE n'existe pas.${NC}" >&2
     exit 1
 fi
 
-echo "Vérification des services listes dans $LISTE..."
+echo "Verification des services listes dans $LISTE..."
 echo "-----------------------------------------------"
-
+# Boucle sur chaque ligne du fichier
 while read -r DAEMON || [ -n "$DAEMON" ]; do
-    # Ignorer les lignes vides et les commentaires
+    # Ignorer les lignes vides ou les commentaires (lignes commencant par #)
     [[ -z "$DAEMON" || "$DAEMON" =~ ^# ]] && continue
 
     echo -n "Service :  $DAEMON : "
-
+ # Verifie si le service est actif
     if systemctl is-active --quiet "$DAEMON"; then
         echo -e "${GREEN} CONFIRMATION : Actif ${NC}"
     else
         echo -e "${RED} ATTENTION : Inactif ${NC}"
-        # Redémarrage automatique (optionnel) :
+        # Redemarrage automatique (optionnel) :
         systemctl restart "$DAEMON" && echo "Redemarre"
     fi
 done < "$LISTE"
 
 echo "-----------------------------------------------"
-echo "✔️ Vérification terminee."
+echo "Verification terminee."
 
